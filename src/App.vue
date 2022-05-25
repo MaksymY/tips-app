a6c9230fc49058ddde93c42517ea7026a62899ae
MaksymY <maksym.yankivskyypro@gmail.com>
2022-05-25 11:41:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Hello</h1>
+</template>
+
+<style>
+@import url('https://fonts.google.com/specimen/Space+Mono');
+
+#app {
+  font-family: 'Space Mono', sans-serif;
+  margin: 0 auto;
+  padding: 0;
+}
+</style>
